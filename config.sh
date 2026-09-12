39b07dd4d2c30727d2b645aac995c19cc59e99d1
baiguai <baiguai.junk@gmail.com>
2026-09-11 22:48:08 -0700

    Initial Push

---
+++ config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Central configuration - edit values here, all scripts pick them up
+
+APP_NAME="kadath"
+
+SOURCES=(
+    "src/main.cpp"
+)
+
+LIBS=(
+    "ftxui::screen"
+    "ftxui::dom"
+    "ftxui::component"
+)
+
+HEADERS=(
+    "src/main.hpp"
+)
